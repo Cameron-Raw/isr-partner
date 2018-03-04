f8677044c7ae40ff1b40f8905eac73b393f99e5d
Cameron-Raw <cameron.raw@me.com>
2018-03-04 17:05:40 +0100

    Todo list now refreshes to show correct number of tasks

--- src/js/todo-list.js
+++ src/js/todo-list.js
@@ -8,8 +8,32 @@ var savedTodoList = require('./json/todo.json');
 var taskInput = document.getElementById('task-input');
 var noTaskWarning = document.getElementById('noTaskWarning');
 var newTaskButton = document.getElementById('task-button');
+var todoListUl = document.getElementById('todo-ul');
 
 console.log("savedTodoList is " + savedTodoList);
+
+function refreshTodoList() {
+  // Clear previous todoList
+  todoListUl.innerHTML = "";
+  // Load existing to do items
+  for( var i = 0; i < savedTodoList.length; i++ ){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(savedTodoList[i].taskTitle));
+    console.log();
+    todoListUl.appendChild(li);
+  }
+
+  console.log(todoListUl.getElementsByTagName("li").length);
+
+  if(todoListUl.getElementsByTagName("li").length > 0){
+    noTaskWarning.style.display = "none";
+  } else {
+    noTaskWarning.style.display = "block";
+  }
+}
+
+refreshTodoList();
+
 
 newTaskButton.addEventListener("click",function(event){
   const modalPath = path.join('file://', __dirname, '../newtask.html');
@@ -23,9 +47,9 @@ newTaskButton.addEventListener("click",function(event){
 ipc.on('addNewTask', function(event, arg){
 
   var newTodoList = savedTodoList;
-  console.log(arg);
+  console.log("arg is " + arg);
   newTodoList.push(arg);
-  console.log(newTodoList);
+  console.log("newtodolist after push is " + newTodoList);
   var newTodoListStr = JSON.stringify(newTodoList);
 
   fs.writeFile('src/js/json/todo.json', newTodoListStr, function(err) {
@@ -33,5 +57,7 @@ ipc.on('addNewTask', function(event, arg){
       console.log("An error ocurred updating JSON file " + err.message);
     }
   });
+
+  refreshTodoList();
 
 });
